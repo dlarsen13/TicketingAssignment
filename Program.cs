e23a7dccb227e1e5d980ccbb58176799fb9ab81a
David <dlarsen13@my.wctc.edu>
2023-01-30 22:47:56 -0600

    Added support for editing the .csv file using StreamWriter

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ while (running)
     Console.WriteLine("Ticketing System");
     Console.WriteLine("Please select an option: ");
     Console.WriteLine($"1. View current tickets \t 2. Edit tickets \t 3. Exit");
-    // Taking and validating user input before proceeding
-
-    // Rip this from an old project, for now just don't fuck up in the meantime
+    // Taking user input before proceeding, using a switch statement to make input validation easier. If nothing is entered, it will read as a blank character and hit the default case
     string userChoice = Console.ReadLine();
     switch (userChoice)
     {
@@ -39,9 +37,12 @@ while (running)
         // Appending to the tickets.csv file
         if (File.Exists(dir + "\\tickets.csv"))
         {
-            using (StreamWriter writer = new StreamWriter(dir + "\\tickets.csv"))
+            using (StreamWriter writer = new StreamWriter(dir + "\\tickets.csv", append: true))
             {
-
+                Console.WriteLine("Please enter what you would like to add to the ticket system");
+                string userInput = Console.ReadLine();
+                // Placing each new append on a new line for a more readable output
+                writer.WriteLine($"\n" + userInput);
             }
         }
         // Else statement if tickets.csv is not found to keep it from imploding
@@ -57,10 +58,8 @@ while (running)
         break;
 
         default:
-        // This might validate input and restart the loop, test it first
+        // Catches bad inputs and brings the user back to the choice menu
         Console.WriteLine("Invalid input, please try again");
         break;
     }
 }
-
-
